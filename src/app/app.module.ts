20e8d98a55c24608e40aa396f74236149e7baf64
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-11-02 00:36:20 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FIREBASE_CONFIG } from './app.firebase.config'
+import { AngularFireDatabase } from "angularfire2/database";
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +19,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +30,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AngularFireDatabase,
+    AngularFireAuth
   ]
 })
-export class AppModule {}
+export class AppModule { }
