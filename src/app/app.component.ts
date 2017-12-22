7fff03aa9fd198623ce3b71d023024081699cad0
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-12-22 13:47:19 +0700

    Pull from profile-setting branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'LoginPage';
+  rootPage: any = 'LoginPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
